e07088955d44fada315be001258c96e924fa609d
SECK, Babacar <babacar.seck.ext@eduserv.fr>
2023-04-02 02:43:48 +0200

    Templates, Interpolation and Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,9 @@ import { Component } from "@angular/core"
     template:`
       <div>
         <h1>{{pageTitle}}</h1>
-        <div>My First Component</div>
+        <pm-products></pm-products>
       </div>
+
     `
 })
 export class AppComponent{
